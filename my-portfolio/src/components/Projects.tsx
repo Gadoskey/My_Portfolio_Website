86db3fb0bca1236d814b7a041685203ed5d8b6b8
Gadoskey <gadoski321@gmail.com>
2025-02-27 12:51:16 +0100

    edit projects component

--- my-portfolio/src/components/Projects.tsx
+++ my-portfolio/src/components/Projects.tsx
@@ -6,56 +6,60 @@ import { projects } from "@/components/Data";
 import { useState } from 'react';
 
 export default function Projects() {
+  const [hoveredProject, setHoveredProject] = useState<number | null>(null);
 
   return (
     <div id="projects" className="pt-16">
       <div className="max-w-4xl mx-auto p-6 bg-slate-900 text-white shadow-md rounded-lg">
         <h2 className="text-[10px] sm:text-[15px] font-semibold text-left border-b pb-2 mb-6">Projects</h2>
         <div className="space-y-6">
-          {projects.slice(0, 6).map((project) => {
-            const [isHovered, setIsHovered] = useState(false);
-
-            return (
+          {projects.slice(0, 6).map((project) => (
+            <div
+              key={project.id}
+              className="flex items-start gap-1 bg-gray-500 p-1 rounded-lg shadow-md"
+            >
               <div
-                key={project.id}
-                className="flex items-start gap-1 bg-gray-500 p-1 rounded-lg shadow-md"
+                className="flex-1 text-left text-[12px] items-start pb-4 p-2 bg-slate-800 rounded-lg hover:bg-slate-900 transition"
+                onMouseEnter={() => setHoveredProject(project.id)}
+                onMouseLeave={() => setHoveredProject(null)}
               >
-                <div
-                  className="flex-1 text-left text-[12px] items-start pb-4 p-2 bg-slate-800 rounded-lg hover:bg-slate-900 transition"
-                  onMouseEnter={() => setIsHovered(true)}
-                  onMouseLeave={() => setIsHovered(false)}
+                <h3 className="text-[10px] sm:text-[14px] font-semibold mb-3">{project.title}</h3>
+                <p className="text-[9px] sm:text-[12px]">{project.description}</p>
+                <p className="text-sm text-gray-700 mt-2">
+                  {project.techUsed.map((tech, index) => (
+                    <span
+                      key={index}
+                      className={`${
+                        hoveredProject === project.id
+                          ? "bg-white text-slate-900"
+                          : "bg-slate-900 text-white"
+                      } text-[7px] sm:text-[12px] px-1.5 sm:p-1 rounded-lg mb-2 sm:mb-3 mt-1 mr-1 sm:mr-2 inline-block transition-all duration-300`}
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </p>
+
+                <a
+                  href={project.link}
+                  target="_blank"
+                  className="text-gray-300 text-[10px] sm:text-[14px] font-medium hover:underline"
                 >
-                  <h3 className="text-[10px] sm:text-[14px] font-semibold mb-3">{project.title}</h3>
-                  <p className="text-[9px] sm:text-[12px]">{project.description}</p>
-                  <p className="text-sm text-gray-700 mt-2">
-                    {project.techUsed.map((tech, index) => (
-                      <span
-                        key={index}
-                        className={`${isHovered ? " bg-white text-slate-900" : "bg-slate-900 text-white"
-                          } text-[7px] sm:text-[12px] px-1.5 sm:p-1 rounded-lg mb-2 sm:mb-3 mt-1 mr-1 sm:mr-2 inline-block transition-all duration-300`}
-                      >
-                        {tech}
-                      </span>
-                    ))}
-                  </p>
-
-                  <a href={project.link} target="_blank" className="text-gray-300 text-[10px] sm:text-[14px] font-medium hover:underline">
-                    View Project →
-                  </a>
-                </div>
-
-                <div className="hidden sm:block w-[150px] h-[200px] overflow-hidden rounded-lg">
-                  <Image
-                    src={project.image}
-                    alt={project.title}
-                    width={150}
-                    height={200}
-                    className="w-full h-full object-fill"
-                  />
-                </div>
+                  View Project →
+                </a>
               </div>
-            );
-          })}
+
+              <div className="hidden sm:block w-[150px] h-[200px] overflow-hidden rounded-lg">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  width={150}
+                  height={200}
+                  className="w-full h-full object-fill"
+                />
+              </div>
+            </div>
+          ))}
         </div>
 
         <div className="mt-6 text-center">
