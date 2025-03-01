38b1c934f1329f6a8d00d5364c542ce8691c3983
Gadoskey <gadoski321@gmail.com>
2025-03-01 08:45:30 +0100

    ddd

--- my-portfolio/src/components/Footer.tsx
+++ my-portfolio/src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div id="contact" className="pt-16">
       <footer className="bg-slate-900 mx-auto p-4 text-center">
         < EmailForm />
-        <div id="contact" className="text-center text-gray-400 text-[10px] mt-12">
+        <div id="contact" className="text-center text-gray-400 text-[10px] lg:text-[14px] mt-12">
           © {new Date().getFullYear()} Yusuf Mustapha Opeyemi. All rights reserved.
         </div>
       </footer>
