31b968e75d9acbffb5cdc67dcadeb965aa891444
Ubuntu <gadoskey@portfolio.fu3cnnrc3hke3ckbqpp5xngmzb.phxx.internal.cloudapp.net>
2025-03-02 11:15:20 +0000

    Automatic Commit

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import ScrollLink from "@/components/ScrollLink";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 w-[70%] sm:w-[30rem] h-[1.7rem] sm:h-[2.6rem] rounded-[16px] border border-slate-950 bg-slate-900 border-opacity-40 bt-white z-50">
+    <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 w-[70%] sm:w-[28rem] h-[1.7rem] sm:h-[2.6rem] rounded-[16px] border border-slate-950 bg-slate-900 border-opacity-40 bt-white z-50">
       <div className="container mx-auto flex justify-center items-center h-full">
         <ul className="flex text-gray-400 gap-4 sm:gap-[25] text-[13px] sm:text-[18px]">
           <li className="flex"> <ScrollLink to="home" className="hover:text-white">Home</ScrollLink> </li>
